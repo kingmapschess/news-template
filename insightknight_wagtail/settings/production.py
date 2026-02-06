aa78dac9f417e47adec730f16ad53dd2287efee9
kingmapschess <hello.gareth@outlook.com>
2026-02-06 09:26:31 +0000

    Fix production settings to load SECRET_KEY and use Postgres

--- insightknight_wagtail/settings/production.py
+++ insightknight_wagtail/settings/production.py
@@ -3,6 +3,14 @@ import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
+
 DEBUG = False
 
 
